3ff0a0df27fbf4cf1cddc806b09ee102f4d34012
luke lin <77534501+sunrenn@users.noreply.github.com>
2022-07-25 00:21:19 +0800

    1

---
+++ src/_index.js
@@ -0,0 +1,11 @@
+import { newNode } from "./__create_newNode.js";
+import { ajaxP5JsFile } from "./__ajax_jsfile.js";
+import { parameters } from "./__url_para.js";
+import { acceler } from "./__anim_acceler.js";
+import { randomChar } from "./__char_random.js";
+import { getStyle } from "./__style_getStyle.js";
+import { sFibonacci } from "./__math_sequences.js";
+
+export {newNode,ajaxP5JsFile,parameters,acceler,randomChar,getStyle,sFibonacci};
+
+
