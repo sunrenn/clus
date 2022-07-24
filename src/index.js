797232c797eac13d66073a2909aed8cd18b44fe4
luke lin <77534501+sunrenn@users.noreply.github.com>
2022-07-25 00:22:48 +0800

    1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 window.onload = ()=>{
     // console.log("index中的comm：");
     // console.log(comm);
-    clusCreate.createdom("h1","hi~","h1a");
+    clusCreate.createdom("h1","hiHhiHhiHhiH~","h1a");
     let txtResult = clusCommon.nowTime();
     h1a.innerHTML = txtResult;
 }
