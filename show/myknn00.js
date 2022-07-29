c1548a9427b5c754d9aef7e7a98eb92b04b9d6e2
lukelin <luke.l.lin@hotmail.com>
2022-07-30 00:23:12 +0800

    hahahahaha

--- show/myknn00.js
+++ show/myknn00.js
@@ -8,20 +8,19 @@ function preload(){
     img = loadImage('../assets/images/canvas.png');
 }
 
-
-
 function setup() {
     createCanvas(400,400);
-    console.log("classifier",classifier);
     classifier.classify(img, gotResult);
     image(img, 0, 0);
 }
 
 function gotResult(error, results){
     if (error) {
+        console.error("error!!!");
         console.error(error);
     }
     else {
+        console.log("results!!!");
         console.log(results);
         createDiv(`Label: ${results[0].label}`);
         createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
--- src/__ajax_jsfile.js
+++ src/__ajax_jsfile.js
@@ -13,10 +13,10 @@ export function ajaxP5JsFile(sId, url) {
         if (oXHR.readyState == 4) {
             replaceJS(sId, url, oXHR.responseText);
         }
-        console.log("getAllResponseHeaders:");
-        console.log(oXHR.getAllResponseHeaders());
-        console.log("readyState:");
-        console.log(oXHR.readyState);
+        // console.log("getAllResponseHeaders:");
+        // console.log(oXHR.getAllResponseHeaders());
+        // console.log("readyState:");
+        // console.log(oXHR.readyState);
     }
     oXHR.open('GET', url, false); //这里要使用异步操作，设计回调程序。
     oXHR.send(null);
